4ce11217fc5985a5e954c0912b4a79b6134148d2
Darshan1124 <darshanthummar9429@gmail.com>
2025-12-07 21:34:35 +0530

    health check up added

---
+++ src/main/java/com/h2/backend/controllers/HealthController.java
@@ -0,0 +1,12 @@
+package com.h2.backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Server is awake!";
+    }
+}
